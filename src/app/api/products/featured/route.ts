5f2c92d892d43588b1554dcd103cd9cee86820e6
Paulo Vitor <pv.marques2010@hotmail.com>
2023-11-17 18:18:17 -0300

    fix: api
    Corrigindo o erro de consumo da API de produtos

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,7 +1,7 @@
 import data from '../data.json'
 
 export async function GET() {
-  const featuredProducts = data.products.filter((product) => product.featured)
+  const featuredProducts = data.products.filter((p) => p.featured)
 
   return Response.json(featuredProducts)
 }
